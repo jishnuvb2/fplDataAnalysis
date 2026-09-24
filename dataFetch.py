027c287d1a391c1648a1a6f402643dbbd1877b7e
“Jishnu” <jishvenugopal@gmail.com>
2026-09-24 23:30:43 +0530

    venv2

--- dataFetch.py
+++ dataFetch.py
@@ -1,3 +1,4 @@
 # this file is used to import the player data and team data
-from config import GAMEWEEK
-print(GAMEWEEK)
+from config import GAMEWEEK, MANAGER_ID
+
+# function to fetch team data from FPL website
