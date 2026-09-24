5a9b34fde280e7ce1573aa817413058651fb056b
“Jishnu” <jishvenugopal@gmail.com>
2026-09-25 02:45:43 +0530

    re updated venv

---
+++ components.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from highlight_text import fig_text
+from mplsoccer import PyPizza, FontManager
+import plotly.express as px
+from plotly.subplots import make_subplots
+from dataFetch import clean_data
+
+def render_1v1_compare():
+    player_df, team_df = clean_data()
+    
